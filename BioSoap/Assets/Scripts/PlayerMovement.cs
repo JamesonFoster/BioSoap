a2b3b8b02e95dbe817777063b81970dd3243c0db
JamesonFoster <jamesonbfoster@gmail.com>
2025-01-25 16:04:37 -0600

    WE GOT JUMPIN BOYS

--- BioSoap/Assets/Scripts/PlayerMovement.cs
+++ BioSoap/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
 	Transform cam;
 	public float speed = 2;
-
-	private void Start()
+    public Rigidbody rb;
+    private void Start()
 	{
 		cam = Camera.main.transform;
 	}
@@ -17,5 +17,11 @@ public class PlayerMovement : MonoBehaviour
 
 		Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
 		transform.Translate(movement * speed * Time.deltaTime, Space.World);
-	}
+
+        if (Input.GetKey("space"))
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(0, 2, 0, ForceMode.Force);
+        }
+    }
 }
