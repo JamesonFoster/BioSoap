bfafcb6542c64bdcc7736ee7e857ab21d34d07f8
JamesonFoster <jamesonbfoster@gmail.com>
2025-01-24 19:38:49 -0600

    Added Folders and Scipt

---
+++ BioSoap/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	public int MovementSpeed = 2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown("w"))
+		{
+			
+		}
+	}
+}
