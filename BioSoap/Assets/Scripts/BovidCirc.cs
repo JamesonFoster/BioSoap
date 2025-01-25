e6ae022a218cb07d4ceb6aac146d7562b9da090f
JamesonFoster <jamesonbfoster@gmail.com>
2025-01-24 22:31:18 -0600

    Added Bovid

---
+++ BioSoap/Assets/Scripts/BovidCirc.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BovidCirc : MonoBehaviour
+{
+    float timeCounter = 0;
+    public float agroTimer = 30;
+    float checker = 0;
+    public float speed = 2;
+    public float changes = 0;
+    public GameObject soapy;
+
+    void Update()
+    {
+        if (checker <= agroTimer)
+        {
+            timeCounter += Time.deltaTime;
+            float x = Mathf.Cos(timeCounter * changes);
+            float y = 10;
+            float z = Mathf.Sin(timeCounter);
+            transform.position = new Vector3(x, y, z);
+            checker += Time.deltaTime;
+        }
+        else
+        {
+            var step = speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, soapy.transform.position, step);
+        }
+
+    }
+}
