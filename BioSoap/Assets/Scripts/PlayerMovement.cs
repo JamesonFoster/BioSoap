e1cd236c9a91b129b5f5a0f5c1170059e00d7794
JamesonFoster <jamesonbfoster@gmail.com>
2025-01-25 17:07:54 -0600

    chara was here

--- BioSoap/Assets/Scripts/PlayerMovement.cs
+++ BioSoap/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,21 @@ public class PlayerMovement : MonoBehaviour
 	Transform cam;
 	public float speed = 2;
     public Rigidbody rb;
+    public bool onground = true;
     private void Start()
 	{
 		cam = Camera.main.transform;
 	}
 
-	void Update()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            onground = true;
+        }
+
+    }
+        void Update()
 	{
 		Vector3 forwardMovement = cam.forward * Input.GetAxis("Vertical");
 		Vector3 horizontalMovement = cam.right * Input.GetAxis("Horizontal");
@@ -18,10 +27,11 @@ public class PlayerMovement : MonoBehaviour
 		Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
 		transform.Translate(movement * speed * Time.deltaTime, Space.World);
 
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && onground == true)
         {
             rb = GetComponent<Rigidbody>();
-            rb.AddForce(0, 2, 0, ForceMode.Force);
+            rb.AddForce(0, 5, 0, ForceMode.Impulse);
+            onground = false;
         }
     }
 }
